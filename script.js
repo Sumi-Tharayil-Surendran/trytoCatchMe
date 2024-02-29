6c2ea04e883053db402ed50a22641e936313d445
Sumi <sumi.tharayil@gmail.com>
2024-02-29 20:02:24 +0000

    add mouseover event function

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const colors=["#F7EC09",
+let colors=["#F7EC09",
 "#F637EC",
 "#37E2D5",
 "#FF4949",
 "#06FF00",
 "#FF0075"]
-const messages=['Try again','u cant catch me😒','still you are close']
+let messages=['Try again','cant catch me😒','u are close']
 
 
 function randomColors(){
@@ -17,6 +17,25 @@ function getrandomNumber(min,max){
    return  Math.floor(Math.random()*(max-min)+min);
 }
 
+//  console.log(randomMessages())
 // console.log(randomColors())
 // console.log(randomMessages())
 // console.log(getrandomNumber(1,500))
+
+ let btn= document.getElementById("btn")
+ btn.addEventListener("mouseover",event=>{
+    let top,bottom,left,right;
+    top = getrandomNumber(1,100)
+    bottom=getrandomNumber(1,100)
+    right=getrandomNumber(1,100)
+    left=getrandomNumber(1,100)
+
+    event.target.style.backgroundColor = `${randomColors()}`
+    event.target.style.color="black";
+
+    event.target.style.top=`${top}px`
+    event.target.style.bottom=`${bottom}px`
+    event.target.style.right=`${right}px`
+    event.target.style.bottom=`${left}px`
+    btn.innerHTML=`${randomMessages()}`;
+ })
